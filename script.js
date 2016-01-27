8383c2d9e912ca827d1099d2ba6a2fbefa275d8c
Sam Corin <samcorin@gmail.com>
2016-01-27 19:30:02 +0900

    Completed user stories

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-/**
-
-User Story: As a user, if I press the wrong button, I am notified that I have done so, and that series of button presses starts again to remind me of the pattern so I can try again.
-
-User Story: As a user, I can see how many steps are in the current series of button presses.
-
-Bonus User Story: As a user, I can play in strict mode where if I get a button press wrong, it notifies me that I have done so, and the game restarts at a new random series of button presses.
-
-Bonus User Story: As a user, the tempo of the game speeds up incrementally on the 5th, 9th and 13th step.
-
- *
- */
-
-
-
 $(document).ready(function() {
-
 
     var patterns = ['tl', 'tr', 'bl', 'br'],
         game = [],
         player = [],
         player_moves = 0,
         i,
+        counter,
         running = true,
+        repeat = false;
         timeout = 600,
-        hiscore = 0;
+        hiscore = 0,
+        lives = 1;
 
     $('.buttons').addClass('unclickable');
     $('.r').text('High Score: ' + hiscore);
@@ -36,12 +23,15 @@ $(document).ready(function() {
     function restart() {
         $('.score').text('Level: 1');
         game = [];
+        lives = 1;
+        timeout = 600;
         player_moves = 0;
+        counter = 0;
         setTimeout(function() {
             $('.buttons').addClass('unclickable');
         }, 1200);
-        $('.start').removeClass('unclickable')
-        $('.start').trigger('click');
+        $('.start').removeClass('unclickable').trigger('click');
+        // $('.start').trigger('click');
     }
 
     $('.restart').on('click', function() {
@@ -50,9 +40,17 @@ $(document).ready(function() {
 
     // Button press animations
     $('.buttons').on('click', function() {
+        
+    	// Audio
+    	var idx = $(this).index();
+        // returns DOM element
+        $('#' + idx)[0].play();
+
+
         if (!running) {
             player.push(this.id);
             player_moves++;
+            $('.progress').text(player_moves + ' / ' + game.length);
             player_move();
         }
 
@@ -97,25 +95,30 @@ $(document).ready(function() {
                 }
 
             } else {
-                console.log('WRONG MOVE!');
-                setTimeout(function() {
-                    alert('Wrong move. Game Over.');
-                }, 500);
-                restart();
+            	// replay the pattern, player has one more chance
+            	if(lives == 1) {
+            		lives = 0;
+            		alert("last chance");
+	                player_moves = 0;
+	                // Is there a way to tie this to running? Don't think so
+	                repeat = true;
+	                setTimeout(function() {
+	                	$('.start').trigger('click');
+	                },500);
+            	} else {
+            		// GAME OVER
+	                setTimeout(function() {
+	                    alert('Time to rethink your strategy.');
+	                }, 500);
+	                restart();
+            	}
             }
         }
     }
 
-    // Alternative: index()
-    $('.buttons').on('click', function() {
-        var idx = $(this).index();
-        // returns DOM element
-        $('#' + idx)[0].play();
-    })
-
-
     // Automatic animations
     $('.start').on('click', function() {
+
         // DISABLE PLAYER CLICK    
         player = [];
         running = true;
@@ -123,30 +126,43 @@ $(document).ready(function() {
         $('.start').addClass('unclickable');
 
         // Pushes a random move to game array
-        rand();
+        // Done like this to allow a repeat of a pattern without pushing a new item to the array
+        if(!repeat) {
+        	rand();
+        }
+        
+        repeat = false;
+
+    	$('.progress').text('0 / ' + game.length);
 
         // Counter is reset so the game replays the pattern from the beginning
-        var counter = 0,
-            // time increment
-            i = 600;
+        counter = 0;
+        i = 600;
 
         // Start a timed loop
-        var timer = setInterval(function() {
+	    var timer = setInterval(function() {
+	        // Triggers 'click' for the correspoding button in the array
+	        $('#' + game[counter]).trigger('click');
+	        counter++;
+	        if(game.length >= 12) {
+	        	timeout = 400;
+	        	i = 400;
+	        } else if(game.length >= 8) {
+	        	timeout = 250;
+	        	i = 250;
+	        } else if(game.length >= 4) {
+	        	timeout = 100;
+	        	i = 100;
+	        }
 
-            // Triggers 'click' for the correspoding button in the array
-            $('#' + game[counter]).trigger('click');
-            counter++;
+	        // Once game array reaches the end, we stop the timer, re-enable player moves, set running to false
+	        if (game[counter] == undefined) {
+	            clearInterval(timer);
+	            $('.buttons').removeClass('unclickable');
+	            player_moves = 0;
+	            running = false;
+	        }
+	    }, i += i);
 
-            // Once game array reaches the end, we stop the timer, re-enable player moves, set running to false
-            if (game[counter] == undefined) {
-                clearInterval(timer);
-                // player_move(counter);
-
-                // ENABLE PLAYER CLICK
-                $('.buttons').removeClass('unclickable');
-                player_moves = 0;
-                running = false;
-            }
-        }, i += i);
     })
 })
